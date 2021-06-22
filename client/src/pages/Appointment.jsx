fd0b10f26c7b6f5577911c0446194d58e6a80fa9
Nahid Sultan <nsn.nahid23@gmail.com>
2021-06-22 15:22:38 +0600

    C:/Program Files/Git/appointment/new page initial setup

--- client/src/pages/Appointment.jsx
+++ client/src/pages/Appointment.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
 
 const Appointment = () => {
-    return <React.Fragment></React.Fragment>;
+    return (
+        <React.Fragment>
+            <div className="appointment">
+                <div className="appointment-form">
+                    <div className="content-title">
+                        <h1>New Appointment</h1>
+                    </div>
+                    <div className="date-area">
+                        <h5>Date:</h5>
+                    </div>
+                    <form>
+                        <div>
+                            <div className="form-group row">
+                                <label htmlFor="inputNameTitle" className="col-sm-4 col-form-label">
+                                    Attendee Name
+                                </label>
+                                <div className="col-sm-8">
+                                    <input type="text" name="name" className="form-control" placeholder="Type your name" required={true} />
+                                </div>
+                            </div>
+                            <div className="button-area">
+                                <button type="submit" className="btn btn-primary mb-2">
+                                    Create Appointment
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </React.Fragment>
+    );
 };
 
 export default Appointment;
