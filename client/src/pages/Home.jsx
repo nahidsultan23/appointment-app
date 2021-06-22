7811cf52ea5bb8b90045810b02645f2b5d93ef97
Nahid Sultan <nsn.nahid23@gmail.com>
2021-06-22 14:47:16 +0600

    Prepare the data from the server for the Homepage use

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,94 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { getAppointmentData } from '../actions/homeActions';
 
 const Home = () => {
+    const [loading, setLoading] = useState(false);
+    const [data, setData] = useState({});
+    const [errorMessage, setErrorMessage] = useState('');
+
+    useEffect(() => {
+        setLoading(true);
+
+        const result = getAppointmentData();
+
+        result
+            .then((res) => {
+                if (res.success) {
+                    let dateNday = [];
+                    let hr0Data = [];
+                    let hr1Data = [];
+                    let hr2Data = [];
+                    let hr3Data = [];
+                    let hr4Data = [];
+
+                    for (let i = 0; i < res.data.length; i++) {
+                        dateNday.push({
+                            date: res.data[i].date,
+                            day: res.data[i].day,
+                            dayName: res.data[i].dayName,
+                        });
+
+                        hr0Data.push({
+                            data: res.data[i].hourData.hr0,
+                            day: res.data[i].day,
+                            dayName: res.data[i].dayName,
+                            date: res.data[i].date,
+                            time: '08:00 - 10:00',
+                        });
+                        hr1Data.push({
+                            data: res.data[i].hourData.hr1,
+                            day: res.data[i].day,
+                            dayName: res.data[i].dayName,
+                            date: res.data[i].date,
+                            time: '10:00 - 12:00',
+                        });
+                        hr2Data.push({
+                            data: res.data[i].hourData.hr2,
+                            day: res.data[i].day,
+                            dayName: res.data[i].dayName,
+                            date: res.data[i].date,
+                            time: '14:00 - 16:00',
+                        });
+                        hr3Data.push({
+                            data: res.data[i].hourData.hr3,
+                            day: res.data[i].day,
+                            dayName: res.data[i].dayName,
+                            date: res.data[i].date,
+                            time: '16:00 - 18:00',
+                        });
+                        hr4Data.push({
+                            data: res.data[i].hourData.hr4,
+                            day: res.data[i].day,
+                            dayName: res.data[i].dayName,
+                            date: res.data[i].date,
+                            time: '18:00 - 20:00',
+                        });
+                    }
+
+                    setData({
+                        dateNday: dateNday,
+                        hr0Data: hr0Data,
+                        hr1Data: hr1Data,
+                        hr2Data: hr2Data,
+                        hr3Data: hr3Data,
+                        hr4Data: hr4Data,
+                    });
+
+                    setErrorMessage('');
+                } else {
+                    setData({});
+                    setErrorMessage(res.errorMessage);
+                }
+            })
+            .catch((err) => {
+                setErrorMessage('Server connection error!');
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    }, []);
+
     return (
         <React.Fragment>
             <div className="homepage">
