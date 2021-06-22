82d0f78370f6510a06b73a9e33940e777c81322e
Nahid Sultan <nsn.nahid23@gmail.com>
2021-06-22 14:31:35 +0600

    Page Not Found in case of an invalid url

--- client/src/pages/PageNotFound.jsx
+++ client/src/pages/PageNotFound.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import image from '../photos/page-not-found.jpg';
 
 const PageNotFound = () => {
-    return <React.Fragment></React.Fragment>;
+    return (
+        <React.Fragment>
+            <div className="page-not-found">
+                <img src={image} alt="Page Not Found" />
+            </div>
+        </React.Fragment>
+    );
 };
 
 export default PageNotFound;
