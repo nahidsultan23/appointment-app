77174d3ff92081289cf0148871c2ddb0d46eab8a
Nahid Sultan <nsn.nahid23@gmail.com>
2021-06-22 16:05:53 +0600

    Appointment Details page initial setup

--- client/src/pages/AppointmentDetails.jsx
+++ client/src/pages/AppointmentDetails.jsx
@@ -1,7 +1,39 @@
 import React from 'react';
 
 const AppointmentDetails = () => {
-    return <React.Fragment></React.Fragment>;
+    return (
+        <React.Fragment>
+            <div className="appointment-details">
+                <div className="appointment-details-area">
+                    <div className="content-title">
+                        <h1>Appointment Details</h1>
+                    </div>
+                    <table className="table table-striped table-dark">
+                        <tbody>
+                            <tr>
+                                <th scope="col">Appointment ID</th>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <th scope="col">Appointment Time</th>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <th scope="col">Booked by</th>
+                                <td>
+                                    <div className="booked-by"></div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="col">Booked on</th>
+                                <td></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </React.Fragment>
+    );
 };
 
 export default AppointmentDetails;
