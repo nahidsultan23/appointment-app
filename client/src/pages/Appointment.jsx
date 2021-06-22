498574a3e27278e5bbf303eed3df8a41671afb39
Nahid Sultan <nsn.nahid23@gmail.com>
2021-06-22 15:30:24 +0600

    Extraction of data from the url, i.e: search query string

--- client/src/pages/Appointment.jsx
+++ client/src/pages/Appointment.jsx
@@ -1,6 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const Appointment = () => {
+const Appointment = (props) => {
+    if (!(props.location.search && props.location.state)) {
+        props.history.push('/home');
+    }
+
+    const [date, setDate] = useState('');
+    const [timeRange, setTimeRange] = useState('');
+    const [name, setName] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    //the 'day' (e.g: 21st, 23rd, etc.) and 'dayName' (e.g: Sunday, Wednesday, etc.) are prepared by the server.
+    //preparing them again from the 'search' query will be repeating those processes as they have already been done in the server.
+    //So, these two have been sent through the 'state'.
+    //to show data extraction from 'search', the 'date' and 'timeRange' will be extracted from it.
+
+    useEffect(() => {
+        if (props.location.search) {
+            let urlSearchParams = new URLSearchParams(props.location.search);
+            let params = Object.fromEntries(urlSearchParams.entries());
+
+            let date = params.start.split('T')[0];
+
+            setDate(date);
+
+            let startTime = params.start.split('T')[1].replace('-', ':');
+            let endTime = params.end.split('T')[1].replace('-', ':');
+
+            let timeRange = startTime + ' - ' + endTime;
+
+            setTimeRange(timeRange);
+        }
+    }, [props.location.search]);
+
     return (
         <React.Fragment>
             <div className="appointment">
@@ -8,9 +40,15 @@ const Appointment = () => {
                     <div className="content-title">
                         <h1>New Appointment</h1>
                     </div>
-                    <div className="date-area">
-                        <h5>Date:</h5>
-                    </div>
+                    {props && props.location && props.location.state ? (
+                        <div className="date-area">
+                            <h5>
+                                Date: {props.location.state.dayName}, {props.location.state.day} - {timeRange}
+                            </h5>
+                        </div>
+                    ) : (
+                        ''
+                    )}
                     <form>
                         <div>
                             <div className="form-group row">
@@ -18,7 +56,16 @@ const Appointment = () => {
                                     Attendee Name
                                 </label>
                                 <div className="col-sm-8">
-                                    <input type="text" name="name" className="form-control" placeholder="Type your name" required={true} />
+                                    <input
+                                        type="text"
+                                        name="name"
+                                        className="form-control"
+                                        placeholder="Type your name"
+                                        required={true}
+                                        onChange={(e) => {
+                                            setName(e.target.value);
+                                        }}
+                                    />
                                 </div>
                             </div>
                             <div className="button-area">
