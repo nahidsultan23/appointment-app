bca9fa7b17802c31b633e83cef4c335baabf158b
Nahid Sultan <nsn.nahid23@gmail.com>
2021-06-22 15:37:56 +0600

    C:/Program Files/Git/appointment/new page error handling

--- client/src/pages/Appointment.jsx
+++ client/src/pages/Appointment.jsx
@@ -62,50 +62,56 @@ const Appointment = (props) => {
     return (
         <React.Fragment>
             <div className="appointment">
-                <div className="appointment-form">
-                    <div className="content-title">
-                        <h1>New Appointment</h1>
+                {errorMessage ? (
+                    <div className="error-message">
+                        <h1>{errorMessage}</h1>
                     </div>
-                    {props && props.location && props.location.state ? (
-                        <div className="date-area">
-                            <h5>
-                                Date: {props.location.state.dayName}, {props.location.state.day} - {timeRange}
-                            </h5>
+                ) : (
+                    <div className="appointment-form">
+                        <div className="content-title">
+                            <h1>New Appointment</h1>
                         </div>
-                    ) : (
-                        ''
-                    )}
-                    <form
-                        onSubmit={(e) => {
-                            submitForm(e);
-                        }}
-                    >
-                        <div>
-                            <div className="form-group row">
-                                <label htmlFor="inputNameTitle" className="col-sm-4 col-form-label">
-                                    Attendee Name
-                                </label>
-                                <div className="col-sm-8">
-                                    <input
-                                        type="text"
-                                        name="name"
-                                        className="form-control"
-                                        placeholder="Type your name"
-                                        required={true}
-                                        onChange={(e) => {
-                                            setName(e.target.value);
-                                        }}
-                                    />
+                        {props && props.location && props.location.state ? (
+                            <div className="date-area">
+                                <h5>
+                                    Date: {props.location.state.dayName}, {props.location.state.day} - {timeRange}
+                                </h5>
+                            </div>
+                        ) : (
+                            ''
+                        )}
+                        <form
+                            onSubmit={(e) => {
+                                submitForm(e);
+                            }}
+                        >
+                            <div>
+                                <div className="form-group row">
+                                    <label htmlFor="inputNameTitle" className="col-sm-4 col-form-label">
+                                        Attendee Name
+                                    </label>
+                                    <div className="col-sm-8">
+                                        <input
+                                            type="text"
+                                            name="name"
+                                            className="form-control"
+                                            placeholder="Type your name"
+                                            required={true}
+                                            onChange={(e) => {
+                                                setName(e.target.value);
+                                            }}
+                                        />
+                                    </div>
+                                </div>
+                                <div className="button-area">
+                                    <button type="submit" className="btn btn-primary mb-2">
+                                        Create Appointment
+                                    </button>
                                 </div>
                             </div>
-                            <div className="button-area">
-                                <button type="submit" className="btn btn-primary mb-2">
-                                    Create Appointment
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                        </form>
+                    </div>
+                )}
             </div>
         </React.Fragment>
     );
